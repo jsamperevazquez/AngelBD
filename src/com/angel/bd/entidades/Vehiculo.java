5f18f1912be8e8690c9c8d70d92c5fe80e1b705f
Jose Ángel <jsamperevazquez@danielcastelao.org>
2021-05-13 02:03:51 +0200

    Creado estructura formulario vehículos

--- src/com/angel/bd/entidades/Vehiculo.java
+++
@@ -1,9 +0,0 @@
-package com.angel.bd.entidades;
-
-/**
- * Creado por @autor: angel
- * El  12 de may. de 2021.
- * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
- **/
-public class Vehiculo {
-}
---
+++ src/com/angel/bd/entidades/vehiculos/gui/Vehiculo.java
@@ -0,0 +1,18 @@
+package com.angel.bd.entidades.vehiculos.gui;
+
+import javax.swing.*;
+
+/**
+ * Creado por @autor: angel
+ * El  12 de may. de 2021.
+ * //-encoding utf8 -docencoding utf8 -charset utf8(Para el javadoc)
+ **/
+public class Vehiculo {
+    private JPanel panel;
+    private JTextField fieldId;
+    private JLabel id_vehiculo;
+    private JTextField fieldMarca;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton registrarVehiculoButton;
+}
